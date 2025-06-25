87393e2a475a7559baee0a32cae3a19f6cf9a721
jason <im.mingxing@qq.com>
2025-06-25 15:33:38 +0800

    更新忽略内容

--- test_code/scope_model_down.py
+++
@@ -1,6 +0,0 @@
-from modelscope.hub.snapshot_download import snapshot_download
-
-model_dir = snapshot_download(
-    model_id='Qwen/Qwen2.5-14B-Instruct-1M',
-    local_dir='~/Qwen/Qwen2.5-14B-DeepSeek-R1-1M'
-)
